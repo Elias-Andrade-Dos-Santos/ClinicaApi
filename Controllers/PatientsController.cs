138ea0d654ba8a2c65efc60cf76713b8736d0b92
Eliezer Andrade Dos Santos <elielias351@gmail.com>
2024-10-21 18:52:58 -0300

    feat: Adicionado uma nova rota UpdatePatient

--- Controllers/PatientsController.cs
+++ Controllers/PatientsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ClinicaApi.DTOs.PatientDTOs;
 using ClinicaApi.Services.Interfaces;
+using ClinicaAPI.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClinicaApi.Controllers
@@ -36,6 +37,25 @@ namespace ClinicaApi.Controllers
             catch (FluentValidation.ValidationException ex)
             {
                var errors = ex.Errors.Select(e => new { Field = e.PropertyName, Error = e.ErrorMessage });
+                return BadRequest(new { Errors = errors });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdatePatient(int id, [FromBody] PatientUpdateDTO patientUpadateDto)
+        {
+            try
+            {
+                await _patientService.UpdatePatientAsync(id, patientUpadateDto);
+                return NoContent();
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (FluentValidation.ValidationException ex)
+            {
+                var errors = ex.Errors.Select(e => new { Field = e.PropertyName, Error = e.ErrorMessage });
                 return BadRequest(new { Errors = errors });
             }
         }
