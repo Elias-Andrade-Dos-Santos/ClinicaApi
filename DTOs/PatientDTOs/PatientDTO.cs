928738eecfa99ba71d50b442229b6e0531604917
Eliezer Andrade Dos Santos <elielias351@gmail.com>
2024-10-21 18:24:33 -0300

    feat: PatientDTO Adicionado

---
+++ DTOs/PatientDTOs/PatientDTO.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinicaApi.DTOs.AddressDTOs;
+
+namespace ClinicaApi.DTOs.PatientDTOs
+{
+    public class PatientDTO
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string CPF { get; set; }
+        public string Gender { get; set; }
+        public AddressDTO Address { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
