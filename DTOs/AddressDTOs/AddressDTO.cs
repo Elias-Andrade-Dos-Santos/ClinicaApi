58cded4efced922d2f894670524a025cb5a9a4d3
Eliezer Andrade Dos Santos <elielias351@gmail.com>
2024-10-21 20:13:59 -0300

    refactor: Colocando o JsonIgnore no atributo PatientId

--- DTOs/AddressDTOs/AddressDTO.cs
+++ DTOs/AddressDTOs/AddressDTO.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ClinicaApi.DTOs.AddressDTOs
@@ -12,6 +13,7 @@ namespace ClinicaApi.DTOs.AddressDTOs
         public string City { get; set; }
         public string State { get; set; }
         public string PostalCode { get; set; }
+        [JsonIgnore]
         public int PatientId { get; set; }
     }
 }
