b65e66c7fd11e9ddfeb0a1348dfa69045aab6f1d
Eliezer Andrade Dos Santos <elielias351@gmail.com>
2024-10-21 22:38:20 -0300

    refactor: Trocado de List para IEnumerable no metodo GetAllAsync

--- Repositories/Interfaces/IAppointmentRepository.cs
+++ Repositories/Interfaces/IAppointmentRepository.cs
@@ -8,7 +8,7 @@ namespace ClinicaApi.Repositories.Interfaces
 {
     public interface IAppointmentRepository
     {
-        Task<List<Appointment>> GetAllAsync();
+        Task<IEnumerable<Appointment>> GetAllAsync();
         Task<Appointment> GetByIdAsync(int id);
         Task AddAsync(Appointment appointment);
         Task UpdateAsync(Appointment appointment);
