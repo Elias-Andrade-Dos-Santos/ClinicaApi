d850caf5f4f1f1c8032157667a7b22e3ea1b43bc
Eliezer Andrade Dos Santos <elielias351@gmail.com>
2024-10-26 03:23:12 -0300

    refactor: refatorando o validator para que quando fot feito o update ele não precise validar novamente a data

--- DTOs/validator/AppointmentValidatorDTO/AppointmentUpdateDTOValidator.cs
+++ DTOs/validator/AppointmentValidatorDTO/AppointmentUpdateDTOValidator.cs
@@ -15,16 +15,15 @@ namespace ClinicaApi.DTOs.validator.AppointmentValidatorDTO
         {
             _patientRepository = patientRepository;
 
-            // Regra: PatientId é obrigatório e o paciente deve existir
+            
             RuleFor(a => a.PatientId)
                 .NotNull().WithMessage("O ID do paciente é obrigatório.")
                 .MustAsync(ExistsInDatabase).WithMessage("Paciente não encontrado.");
 
-            // Regra: Data e hora do atendimento deve ser uma data futura
-            RuleFor(a => a.DateTime)
-                .GreaterThan(DateTime.Now).WithMessage("A data do atendimento deve ser uma data futura.");
+            
+            RuleFor(a => a.DateTime);
 
-            // Regra: Descrição é obrigatória e deve ter no mínimo 10 caracteres
+            
             RuleFor(a => a.Description)
                 .NotEmpty().WithMessage("A descrição do atendimento é obrigatória.")
                 .MinimumLength(10).WithMessage("A descrição deve ter no mínimo 10 caracteres.");
@@ -32,7 +31,7 @@ namespace ClinicaApi.DTOs.validator.AppointmentValidatorDTO
 
         private async Task<bool> ExistsInDatabase(int patientId, CancellationToken cancellationToken)
         {
-            // Verifica se o paciente existe no banco de dados usando o repositório
+            
             return await _patientRepository.GetByIdAsync(patientId) != null;
         }
         
