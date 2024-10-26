0b78bc7ae1983e1b4793d6ea24fac5035c61245d
Eliezer Andrade Dos Santos <elielias351@gmail.com>
2024-10-26 02:41:55 -0300

    refctor: refatorando a validação para validar apenas data até o dia atual

--- DTOs/validator/AppointmentValidatorDTO/AppointmentPostDTOValidator.cs
+++ DTOs/validator/AppointmentValidatorDTO/AppointmentPostDTOValidator.cs
@@ -20,11 +20,11 @@ namespace ClinicaApi.DTOs.validator.AppointmentValidatorDTO
                 .NotNull().WithMessage("O ID do paciente é obrigatório.")
                 .MustAsync(ExistsInDatabase).WithMessage("Paciente não encontrado.");
 
-            // Regra: Data e hora do atendimento deve ser uma data futura
+            
             RuleFor(a => a.DateTime)
-                .GreaterThan(DateTime.Now).WithMessage("A data do atendimento deve ser uma data futura.");
+                .LessThanOrEqualTo(DateTime.Now).WithMessage("A data do atendimento deve ser até a data e hora atual.");
 
-            // Regra: Descrição é obrigatória e deve ter no mínimo 10 caracteres
+            
             RuleFor(a => a.Description)
                 .NotEmpty().WithMessage("A descrição do atendimento é obrigatória.")
                 .MinimumLength(10).WithMessage("A descrição deve ter no mínimo 10 caracteres.");
@@ -32,7 +32,7 @@ namespace ClinicaApi.DTOs.validator.AppointmentValidatorDTO
 
         private async Task<bool> ExistsInDatabase(int patientId, CancellationToken cancellationToken)
         {
-            // Verifica se o paciente existe no banco de dados usando o repositório
+           
             return await _patientRepository.GetByIdAsync(patientId) != null;
         }
     }
